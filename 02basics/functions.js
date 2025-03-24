0875c988c207c69bf318b6d1cef5968e8aee58ad
Lukas Resgalla <lukasresgalla@gmail.com>
2025-03-24 16:27:04 -0300

    add functions

--- 02basics/functions.js
+++ 02basics/functions.js
@@ -8,3 +8,25 @@ console.log(add2(3));
 function getUpperCase(str) {
     return str.toUpperCase();
 }
+console.log(getUpperCase('hello'));
+function signUp(name, email, password, isPaid) { }
+signUp('name', 'email', 123, true);
+// problema!
+function soma(num) {
+    return 'hello';
+}
+function somaCerta(num) {
+    return 1;
+}
+// em arrow function
+var darOi = function (a) {
+    return a;
+};
+// void
+function consoleError(errormsg) {
+    console.log(errormsg);
+}
+// never
+function handleError(errormsg) {
+    throw new Error(errormsg);
+}
--- 02basics/functions.ts
+++ 02basics/functions.ts
@@ -10,3 +10,32 @@ console.log(add2(3))
 function getUpperCase(str: string): string {
     return str.toUpperCase()
 }
+
+console.log(getUpperCase('hello'))
+
+function signUp(name: string, email: string, password: number, isPaid: boolean) {}
+signUp('name', 'email', 123, true)
+
+// problema!
+function soma(num: number){
+    return 'hello'
+}
+
+function somaCerta(num: number): number{
+    return 1
+}
+
+// em arrow function
+const darOi = (a: string): string => {
+    return a
+}
+
+// void
+function consoleError(errormsg: string): void {
+    console.log(errormsg)
+}
+
+// never
+function handleError(errormsg: string): never {
+    throw new Error(errormsg)
+}
