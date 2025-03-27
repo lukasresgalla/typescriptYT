05df73d1cb96d183dd67a3d7a2701607c9b39e69
Lukas Resgalla <lukasresgalla@gmail.com>
2025-03-27 16:30:42 -0300

    adding examples

---
+++ 04/exemplo.ts
@@ -0,0 +1,40 @@
+
+
+//numero
+let numero: number
+
+let numero2 = 3
+
+//string
+let nome: string
+let nome2 = "Chora-dev"
+
+//boolean
+let ativo: boolean
+let ativo2 = true
+
+//undefined
+let indefinido: undefined
+let indefinido2 = undefined
+
+//null
+let nulo: null
+let nulo2 = null
+
+//void
+let semRetorno: void
+
+function semRetornoFuncao(): void {
+    console.log('sem retorno')
+}
+
+//array
+let arrayNumeros: number[]
+let arrayNumeros2: Array<number>
+let arrayNumeros3 = [1, 2, 3]
+
+//tupla
+let tupla: [number, string]
+let tupla2: [number, string, boolean]
+
+
